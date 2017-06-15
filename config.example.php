3aa7c2b5efbd7ea1eeacd5c0ce8b13d789ede58a
Hoang Vu Nam <namhv@rikkeisoft.com>
2017-06-15 14:49:51 +0700

    Configured

--- config.example.php
+++ config.example.php
@@ -1,6 +1,6 @@
 
 <?php
 // see README.md for an explanation of these config params
-$TWILIO_ACCOUNT_SID = 'your account sid here';
-$TWILIO_API_KEY = 'your API key here';
-$TWILIO_API_SECRET = 'your API secret here';
+$TWILIO_ACCOUNT_SID = '0285dfd07d2bcca7e9b00eee60cdd293';
+$TWILIO_API_KEY = '0285dfd07d2bcca7e9b00eee60cdd293';
+$TWILIO_API_SECRET = '0285dfd07d2bcca7e9b00eee60cdd293';
